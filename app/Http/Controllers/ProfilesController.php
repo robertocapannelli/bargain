cbfb93b2292720675048b79a88bdc84208c74949
Roberto Capannelli <roberto.capannelli@gmail.com>
2020-03-18 00:11:29 +0100

    Update: changed profile controller view destination

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,7 +10,7 @@ class ProfilesController extends Controller {
 
         $user = User::findOrFail( $user );
 
-        return view( 'home', [
+        return view( 'profiles.index', [
             'user' => $user
         ] );
     }
