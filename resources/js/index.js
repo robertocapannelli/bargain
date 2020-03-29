7f72ed321a0a2f5e8046f9bca8c2d26ad5cecbe0
Roberto Capannelli <roberto.capannelli@gmail.com>
2020-03-29 23:46:10 +0200

    Fix: moved all react bootstrap theme files from js/components to js/

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import AppTheme from "./AppTheme";
+
+ReactDOM.render(<AppTheme />, document.getElementById("root"));
